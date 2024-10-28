8f1455d56860c1606ba16c052264b0fd8cdae3ea
Juan Silva Suarez <juanseba.96@hotmail.com>
2024-10-28 19:44:58 -0300

    agregar vehiculo.py y automovil.py

---
+++ automovil.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from vehiculo import Vehiculo
+
+class Automovil(Vehiculo):
+    def __init__(self, marca, modelo, numero_ruedas, velocidad, cilindrada):
+        super().__init__(marca, modelo, numero_ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
---
+++ vehiculo.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+class Vehiculo:
+    marca: str
+    modelo: str
+    numero_ruedas:int
+
