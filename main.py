df7ba9612df0407c7a219ac6738c56afa12e9773
Juan Silva Suarez <juanseba.96@hotmail.com>
2024-10-30 17:13:44 -0300

    terminada la mayoria de funcionalidades

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
 from vehiculo import Vehiculo
-from automovil import Automovil
-from carga import Carga
-from particular import Particular
-from bicicleta import Bicicleta
-from motocicleta import Motocicleta
+from vehiculo import Automovil
+from vehiculo import Carga
+from vehiculo import Particular
+from vehiculo import Bicicleta
+from vehiculo import Motocicleta
 
+# PARTE 1
+print("---INICIO PARTE 1---")
+# cantidad_ing = int(input("Cuantos Vehiculos desea insertar: "))
+# vehiculos = []
+# for i in range(cantidad_ing):
+#     print(f"Datos del automóvil {i+1}")
+#     marca = input("Inserte la marca del automóvil: ")
+#     modelo = input("Inserte el modelo del automóvil: ")
+#     numero_ruedas = int(input("Inserte el número de ruedas: "))
+#     velocidad = float(input("Inserte la velocidad en km/h:"))
+#     cilindrada = int(input("Inserte el cilindraje en cc: "))
+#     auto = Automovil(marca, modelo, numero_ruedas, velocidad, cilindrada)
+#     vehiculos.append(auto)
+# print("Imprimiendo por pantalla los Vehiculos")
+# for i in vehiculos:
+#     print(i)
 
+print("---INICIO PARTE 2---")
+# PARTE 2
 particular = Particular("Ford", "Fiesta", 4, "180", "500", 5)
 carga = Carga("Daft Trucks", "G 38", 10, 120, "1000", "20000")
 bicicleta = Bicicleta("Shimano", "MT Ranger", 2, "Carrera")
 motocicleta = Motocicleta("BMW", "F800s", 2, "Deportiva", "2T", "Doble Viga", 21)
 
 print(particular)
+print(carga)
+print(bicicleta)
+print(motocicleta)
 
-print(isinstance(motocicleta,Vehiculo))
+print(
+    "Motocicleta es instacion con relación a Vehiculo: ",
+    isinstance(motocicleta, Vehiculo),
+)
+print(
+    "Motocicleta es instacion con relación a Automovil: ",
+    isinstance(motocicleta, Automovil),
+)
+print(
+    "Motocicleta es instacion con relación a Vehiculo particular: ",
+    isinstance(motocicleta, Particular),
+)
+print(
+    "Motocicleta es instacion con relación a Vehiculo de carga: ",
+    isinstance(motocicleta, Carga),
+)
+print(
+    "Motocicleta es instacion con relación a Bicicleta: ",
+    isinstance(motocicleta, Bicicleta),
+)
+print(
+    "Motocicleta es instacion con relación a Motocicleta: ",
+    isinstance(motocicleta, Motocicleta),
+)
+
+particular.guardar_datos_csv()
+carga.guardar_datos_csv()
+bicicleta.guardar_datos_csv()
+motocicleta.guardar_datos_csv()
+
+particular.leer_datos_csv()
--- vehiculo.py
+++ vehiculo.py
@@ -1,15 +1,22 @@
 from dataclasses import dataclass
+import csv
+from typing import List
 
 
 @dataclass
 class Vehiculo:
-    marca: str
-    modelo: str
-    numero_ruedas: int
+    _marca: str
+    _modelo: str
+    _numero_ruedas: int
+
+    def __init__(self, marca, modelo, numero_ruedas):
+        self._marca = marca
+        self._modelo = modelo
+        self._numero_ruedas = numero_ruedas
 
     @property
     def marca(self):
-        return self.marca
+        return self._marca
 
     @marca.setter
     def marca(self, marca):
@@ -17,7 +24,7 @@ class Vehiculo:
 
     @property
     def modelo(self):
-        return self.modelo
+        return self._modelo
 
     @modelo.setter
     def modelo(self, modelo):
@@ -25,22 +32,77 @@ class Vehiculo:
 
     @property
     def numero_ruedas(self):
-        return self.numero_ruedas
+        return self._numero_ruedas
 
     @numero_ruedas.setter
     def numero_ruedas(self, numero_ruedas):
         self.numero_ruedas = numero_ruedas
 
+    def guardar_datos_csv(self):
+        try:
+            with open("vehiculos.csv", "a", newline='') as file:
+                datos = [(self.__class__, self.__dict__)]
+                archivo_csv = csv.writer(file)
+                archivo_csv.writerows(datos)
+                print("Datos guardados en archivo csv")
+        except Exception as e:
+            print("Error al guardar datos en archivo csv", e)
+
+    def leer_datos_csv(self):
+        # datos = []
+        particulares=[]
+        cargas=[]
+        bicicletas=[]
+        motocicletas=[]
+        try:
+            with open("vehiculos.csv", "r") as file:
+                archivo_csv = csv.reader(file)
+                for row in archivo_csv:
+                    # if row[0] == str(self.__class__):
+                    #     datos.append(row[1])
+                    #     print("Datos leidos del archivo csv")
+                    # else:
+                    #     print("No hay datos")
+                    if row[0]=="<class 'vehiculo.Particular'>":
+                        particulares.append(row[1])
+                    elif row[0]=="<class 'vehiculo.Carga'>":
+                        cargas.append(row[1])
+                    elif row[0]=="<class 'vehiculo.Bicicleta'>":
+                        bicicletas.append(row[1])
+                    elif row[0]=="<class 'vehiculo.Motocicleta'>":
+                        motocicletas.append(row[1])
+                    else:
+                        print("No hay datos")
+                print("Lista de Vehiculos Particular")
+                for i in particulares:
+                    print(i)
+                print("Lista de Vehiculos de Carga")
+                for i in cargas:
+                    print(i)
+                print("Lista de Vehiculos Bicicleta")
+                for i in bicicletas:
+                    print(i)
+                print("Lista de Vehiculos Motocicleta")
+                for i in motocicletas:
+                    print(i)
+
+        except Exception as e:
+            print("Error al leer datos del archivo csv", e)
+        # return datos
+
 
 class Automovil(Vehiculo):
     def __init__(self, marca, modelo, numero_ruedas, velocidad, cilindrada):
         super().__init__(marca, modelo, numero_ruedas)
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
+        self._velocidad = velocidad
+        self._cilindrada = cilindrada
+
+    def __str__(self):
+        return f"Marca {self.marca}, Modelo {self.modelo}, {self.numero_ruedas} ruedas {self.velocidad} Km/h, {self.cilindrada} cc"
 
     @property
     def velocidad(self):
-        return self.velocidad
+        return self._velocidad
 
     @velocidad.setter
     def velocidad(self, velocidad):
@@ -48,7 +110,7 @@ class Automovil(Vehiculo):
 
     @property
     def cilindrada(self):
-        return self.cilindrada
+        return self._cilindrada
 
     @cilindrada.setter
     def cilindrada(self, cilindrada):
@@ -60,11 +122,14 @@ class Particular(Automovil):
         self, marca, modelo, numero_ruedas, velocidad, cilindrada, numero_puestos
     ):
         super().__init__(marca, modelo, numero_ruedas, velocidad, cilindrada)
-        self.numero_puestos = numero_puestos
+        self._numero_puestos = numero_puestos
+
+    def __str__(self):
+        return f"Marca {self.marca}, Modelo {self.modelo}, {self.numero_ruedas} ruedas {self.velocidad} Km/h, {self.cilindrada} cc, Puestos: {self.numero_puestos}"
 
     @property
     def numero_puestos(self):
-        return self.numero_puestos
+        return self._numero_puestos
 
     @numero_puestos.setter
     def numero_puestos(self, numero_puestos):
@@ -74,11 +139,14 @@ class Particular(Automovil):
 class Carga(Automovil):
     def __init__(self, marca, modelo, numero_ruedas, velocidad, cilindrada, peso_carga):
         super().__init__(marca, modelo, numero_ruedas, velocidad, cilindrada)
-        self.peso_carga = peso_carga
+        self._peso_carga = peso_carga
+
+    def __str__(self):
+        return f"Marca {self.marca}, Modelo {self.modelo}, {self.numero_ruedas} ruedas {self.velocidad} Km/h, {self.cilindrada} cc,  Carga:{self.peso_carga} kg"
 
     @property
     def peso_carga(self):
-        return self.peso_carga
+        return self._peso_carga
 
     @peso_carga.setter
     def peso_carga(self, peso_carga):
@@ -88,11 +156,14 @@ class Carga(Automovil):
 class Bicicleta(Vehiculo):
     def __init__(self, marca, modelo, numero_ruedas, tipo):
         super().__init__(marca, modelo, numero_ruedas)
-        self.tipo = tipo
+        self._tipo = tipo
+
+    def __str__(self):
+        return f"Marca {self.marca}, Modelo {self.modelo}, {self.numero_ruedas} ruedas, Tipo: {self.tipo}"
 
     @property
     def tipo(self):
-        return self.tipo
+        return self._tipo
 
     @tipo.setter
     def tipo(self, tipo):
@@ -100,23 +171,26 @@ class Bicicleta(Vehiculo):
 
 
 class Motocicleta(Bicicleta):
-    def __init__(self, marca, modelo, numero_ruedas, tipo, nro_radios, cuadro, motor):
+    def __init__(self, marca, modelo, numero_ruedas, tipo, motor, cuadro, nro_radios):
         super().__init__(marca, modelo, numero_ruedas, tipo)
-        self.nro_radios = nro_radios
-        self.cuadro = cuadro
-        self.motor = motor
+        self._motor = motor
+        self._cuadro = cuadro
+        self._nro_radios = nro_radios
+
+    def __str__(self):
+        return f"Marca {self.marca}, Modelo {self.modelo}, {self.numero_ruedas} ruedas, Tipo: {self.tipo}, Motor: {self.motor}, Cuadro: {self.cuadro}, Nro radios: {self.nro_radios}"
 
     @property
     def nro_radios(self):
-        return self.nro_radios
+        return self._nro_radios
 
     @nro_radios.setter
     def nro_radios(self, nro_radios):
-        self.nro_radios = nro_radios
+        self._nro_radios = nro_radios
 
     @property
     def cuadro(self):
-        return self.cuadro
+        return self._cuadro
 
     @cuadro.setter
     def cuadro(self, cuadro):
@@ -124,7 +198,7 @@ class Motocicleta(Bicicleta):
 
     @property
     def motor(self):
-        return self.motor
+        return self._motor
 
     @motor.setter
     def motor(self, motor):
